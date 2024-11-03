3d83655b32b69b01239805721ea4f9b89f1931b4
Varji <108232155+Varji@users.noreply.github.com>
2024-11-03 21:10:26 +0400

    Update main.py
    Столбец mts мы смотрим в заключении
    1.не выявлено
    а. не выявлено
    б. mts + текст(если выявлено).
    
    !ещё есть формулировка susp.mts.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
+import re
 
 df = pd.read_excel("c61_fio1_uslugs.xls")
-
 
 def find_data_by_doctor(doctor_names):
     doctor_names = doctor_names.split(",")
@@ -11,29 +11,19 @@ def find_data_by_doctor(doctor_names):
     data = []
     for tkey in doctor_df['tkey'].unique():
         row = doctor_df[doctor_df['tkey'] == tkey].iloc[-1]
-
         psa = None
-        date = None
+        date = row["d_prm"]
         volume = None
         hypoechoic_nodes = "не обнаружены"
         invasion = "не выявлено"
         invasion_location = None
         mts = "не выявлено"
         mts_location = None
-
-        protocol = row["Протокол"].lower() 
-
-        if "пса" in protocol:
-            psa_start = protocol.find("пса")
-            psa_end = protocol.find("нг", psa_start)
-            psa = row["Протокол"][psa_start:psa_end].strip()
-
-        date = row["d_prm"]
-
-        if "объем" in protocol:
-            volume_start = protocol.find("объем")
-            volume_end = protocol.find("см3", volume_start)
-            volume = row["Протокол"][volume_start:volume_end].strip()
+        protocol = row["Протокол"].lower()
+        psa_match = re.search(r"пса\s*([\d.]+)\s*нг", protocol)
+        psa = psa_match.group(1) if psa_match else None
+        volume_match = re.search(r"объем\s*([\d.]+)\s*см3", protocol)
+        volume = volume_match.group(1) if volume_match else None
 
         if "гипоэхоген" in protocol:
             hypoechoic_nodes = "обнаружены"
@@ -45,19 +35,20 @@ def find_data_by_doctor(doctor_names):
                 invasion = "выявлено"
                 invasion_start = protocol.find("инвази") + 7
                 invasion_end = protocol.find(".", invasion_start)
-                invasion_location = row["Протокол"][invasion_start:invasion_end].strip() if invasion_end != -1 else row["Протокол"][
-                                                                                                invasion_start:].strip()
+                invasion_location = row["Протокол"][invasion_start:invasion_end].strip() if invasion_end != -1 else row["Протокол"][invasion_start:].strip()
 
         if "mts" in protocol:
             if "не выявлено" in protocol[protocol.find("mts"):]:
                 mts = "не выявлено"
+            elif "susp.mts" in protocol:
+                mts = "susp.mts"
             else:
                 mts = "выявлено"
                 mts_start = protocol.find("mts") + 3
                 mts_end = protocol.find(".", mts_start)
-                mts_location = row["Протокол"][mts_start:mts_end].strip() if mts_end != -1 else row["Протокол"][
-                                                                                                mts_start:].strip()
-        if any([psa, volume, hypoechoic_nodes == "обнаружены", invasion == "выявлено", mts == "выявлено"]):
+                mts_location = row["Протокол"][mts_start:mts_end].strip() if mts_end != -1 else row["Протокол"][mts_start:].strip()
+
+        if any([psa, volume, hypoechoic_nodes == "обнаружены", invasion == "выявлено", mts != "не выявлено"]):
             data.append({
                 "tkey": row['tkey'],
                 "idvisit": row['idvisit'],
@@ -75,7 +66,6 @@ def find_data_by_doctor(doctor_names):
     result_df = pd.DataFrame(data)
 
     return result_df
-
 
 result_df = find_data_by_doctor("Волков В.М.,ВОЛКОВ ВЛАДИСЛАВ МИХАЙЛОВИЧ")
 
